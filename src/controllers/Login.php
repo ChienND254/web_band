21f579e58f9ea331ec5a7f32afd5ed0e0e18c97f
chiennd <104510473+ChienND254@users.noreply.github.com>
2023-12-22 02:13:06 +0700

    fix 1

--- src/controllers/Login.php
+++ src/controllers/Login.php
@@ -14,21 +14,32 @@ class Login extends Controller
         $email = $_POST['email'];
         $password = $_POST['password'];
         $user = $this->model_user->findByEmail($email);
-        if (isset($user[0])) {
-            if ($password == $user[0]['password']) {
-                $_SESSION['id'] = $user[0]['id'];
-                $_SESSION['email'] = $user[0]['email'];
-                $_SESSION['role'] = $user[0]['role'];
-                Header("Location:" . _WEB_ROOT . "/home");
+        if ($email == '' && $password == '') {
+            $this->data['err_email'] = "Email is required";
+            $this->data['err_password'] = "Password is required";
+            $this->render('login/login', $this->data);
+        } else {
+            if (isset($user[0])) {
+                if ($password == '') {
+                    $this->data['err_email'] = "";
+                    $this->data['err_password'] = "Password is required";
+                    $this->render('login/login', $this->data);
+                } else {
+                    if (password_verify($password, $user[0]['password'])) {
+                        $_SESSION['id'] = $user[0]['id'];
+                        $_SESSION['role'] = $user[0]['role'];
+                        Header("Location:" . _WEB_ROOT . "/home");
+                    } else {
+                        $this->data['err_email'] = "";
+                        $this->data['err_password'] = "Wrong password";
+                        $this->render('login/login', $this->data);
+                    }
+                }
             } else {
-                $this->data['err_email'] = "";
-                $this->data['err_password'] = "Wrong password";
+                $this->data['err_email'] = "Email not exist";
+                $this->data['err_password'] = "";
                 $this->render('login/login', $this->data);
             }
-        } else {
-            $this->data['err_email'] = "Email not exist";
-            $this->data['err_password'] = "";
-            $this->render('login/login', $this->data);
         }
     }
 
--- src/controllers/User.php
+++ src/controllers/User.php
@@ -13,26 +13,48 @@ class User extends Controller
 
     public function index()
     {
-        $dataUser  = $this->model_user->getDetail($_SESSION['id']);
-        $this->data['user_context'] = $dataUser;
-
-        $this->render('user/user',$this->data);
+        if (isset($_SESSION['id'])) {
+            $dataUser  = $this->model_user->getDetailModel($_SESSION['id']);
+            $this->data['user_context'] = $dataUser;
+            $this->render('user/user', $this->data);
+        } else {
+            Header("Location:" . _WEB_ROOT . "/login");
+        }
     }
 
     public function logout()
     {
         session_destroy();
-        Header("Location:" . _WEB_ROOT . "/home");
+        Header("Location:" . _WEB_ROOT . "/login");
     }
 
-    public function fileupload()
+    public function update()
     {
-        $this->file->fileUpload('user/',"fileUpLoad");
+        $dataUser  = $this->model_user->getDetailModel($_SESSION['id']);
+        $email = $_POST['email'];
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $dataUser['email'] = $email;
+        $dataUser['name'] = $name;
+        $dataUser['phone'] = $phone;
+        $dataUser['address'] = $address;
+        $this->model_user->updateModel($_SESSION['id'], $dataUser);
+        Header("Location:" . _WEB_ROOT . "/user");
+    }
+
+    public function change_avatar()
+    {
+        $filename = $this->file->fileUpload('user/', 'fileToUpload')[1];
+        $dataUser  = $this->model_user->getDetailModel($_SESSION['id']);
+        $dataUser['img_url'] = $filename;
+        $this->model_user->updateModel($_SESSION['id'], $dataUser);
+
         $this->index();
     }
 
     public function readfile($imgName)
     {
-        $this->file->getFileContent('user/'.$imgName);
+        $this->file->getFileContent('user/' . $imgName);
     }
 }
