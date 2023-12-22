0a8532f52eee7f2432488722b342f40d546100ee
chiennd <104510473+ChienND254@users.noreply.github.com>
2023-12-22 19:44:59 +0700

    add tour list-detail

--- src/controllers/Tour.php
+++ src/controllers/Tour.php
@@ -3,9 +3,11 @@ class Tour extends Controller
 {
     public $data = [];
     public $model_tour;
+    public $file;
 
     public function __construct()
     {
+        $this->file = new FileUpload();
         $this->model_tour = $this->model('TourModel');
     }
 
@@ -13,16 +15,23 @@ class Tour extends Controller
     {
         echo "Danh sách Tour diễn";
     }
-    
-    public function list() {
-        $dataList  = $this->model_tour->getList();
+
+    public function list()
+    {
+        $dataList  = $this->model_tour->getListModel();
         $this->data['tour_list'] = $dataList;
-        $this->render('tour/list',$this->data);
+        $this->render('tour/list', $this->data);
     }
 
-    public function detail($id) {
-        $dataDetail  = $this->model_tour->getDetail($id);
+    public function detail($id)
+    {
+        $dataDetail  = $this->model_tour->getDetailModel($id);
         $this->data['tour_detail'] = $dataDetail;
-        $this->render('tour/detail',$this->data);
+        $this->render('tour/detail', $this->data);
+    }
+
+    public function readfile($imgName)
+    {
+        $this->file->getFileContent('tour/' . $imgName);
     }
 }
